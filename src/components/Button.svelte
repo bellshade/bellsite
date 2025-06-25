db9fd6921059233aee354c4ec265e421580d35be
Rendra Kusuma Blikololo <rendrakusumab@gmail.com>
2025-06-25 12:45:14 +0700

    children prop in Button can now be null

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -1,7 +1,13 @@
 <script lang="ts">
-	const { text, className = '', children } = $props();
+	import type { Snippet } from 'svelte';
+	type Props = {
+		text?: string;
+		className?: string;
+		children?: Snippet;
+	};
+	const { text = '', className = '', children }: Props = $props();
 </script>
 
 <button class="{className} flex gap-2 rounded-lg bg-black px-6 py-3 text-white shadow-lg"
-	>{@render children()}{text}</button
+	>{@render children?.()}{text}</button
 >
